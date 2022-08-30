5c31a871da7c4132c1e3fc2cb986d936be526102
mtugrulesin <mtugrulesin@gmail.com>
2022-08-30 10:39:44 +0300

    Initial Commit

---
+++ Body Worn App/Body_Worn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Worn_AppApp.swift
+//  Body Worn App
+//
+//  Created by Mehmet Tuğrul EŞİN on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Body_Worn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
